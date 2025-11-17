d353c3223ae9f7ef355d39e421fff9631abcd542
anuragprasoon <anuragprasoon23@gmail.com>
2025-11-18 01:01:09 +0530

    mixpanel integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,16 @@ import ShreddedCarousel from "@/components/carousel";
 import Footer from "@/components/footer";
 import productData from '@/data/products.json';
 import { Product, ProductWithSingleImage } from "@/types/product";
+import mixpanel from "mixpanel-browser";
+
+
 
 export default function Home() {
+  mixpanel.init('edae966047b0acb149ae12f73980fd65', {
+    autocapture: true,
+    record_sessions_percent: 100,
+  })
+
   return (<>
     <DesktopNavbar />
     <ShreddedCarousel/>
@@ -30,6 +38,11 @@ export default function Home() {
         image: p.images[0]
       }))}
     />
+    <div className="fixed absolute bottom-2 right-2">
+        <a href="https://api.whatsapp.com/send/?phone=917003634432&text=Hi">
+          <img src="https://res.cloudinary.com/dibrmj6nh/image/upload/v1747946474/Frame_1597884222_xnchxt.png" className="w-[50px]"/>
+        </a>
+    </div>
     <Footer/>
     
   </>
